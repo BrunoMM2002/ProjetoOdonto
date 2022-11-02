84db3aaaf24252ad37fe67b36c20ff30ebd31f2d
BrunoMM2002 <brunommachado2002@gmail.com>
2022-11-02 18:37:30 -0300

    alterações finais

---
+++ README.md
@@ -0,0 +1,3 @@
+# Projeto Odonto Loja Muller
+
+Site desenvolvido com HTML, CSS (Framework tailwind), JS (Utilizando axios para fazer requisição). Backend simples com conexão ao banco mysql e query para buscar se já existe email cadastrado no banco.
---
+++ backend/index.php
@@ -0,0 +1,37 @@
+<?php
+        // Conexão com banco
+        $servername = "localhost:3306";
+        $username = "root";
+        $password = "";
+        $dbname = "contato";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+        }
+
+        // Variaveis que vem do frontend
+        $nome = strval($_GET['nome']);
+        $email = strval($_GET['email']);
+        $telefone = strval($_GET['telefone']);
+        $mensagem = strval($_GET['mensagem']);
+
+        $sql = "SELECT * FROM contato WHERE email = '$email'";
+
+        $result = $conn->query($sql);
+
+        if($result->num_rows > 0){
+                echo "Email já registrado";
+        } else {
+                $sql = "INSERT INTO contato (nome, telefone, email, mensagem) VALUES ('$nome', '$telefone', '$email', '$mensagem')";
+
+                if ($conn->query($sql) === TRUE) {
+                        echo "Cadastrado com sucesso";
+                } else {
+                        echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+        }
+
+        $conn->close();
+?>
